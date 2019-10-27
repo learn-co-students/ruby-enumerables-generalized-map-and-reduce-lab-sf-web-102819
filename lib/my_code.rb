779a434ef9687ec84eae05818701c37f6f806da7
Michael Espina <michael_espina@yahoo.com>
2019-10-27 04:07:24 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
-# Your Code Here
+def map(array)
+  new = []
+  index = 0
+  while index < array.length
+    new.push(yield(array[index]))
+    index += 1
+  end
+  new
+end
+
+p map([1,2,3]){|x| x * -1}
+
+
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    index = 0
+  else
+    accum = s[0]
+    index = 1
+  end
+  while index < s.length
+    accum = yield(accum, s[index])
+    index += 1
+  end
+  accum
+end
+
+p reduce([1,2,3], 100){|memo, x|  x + memo }
